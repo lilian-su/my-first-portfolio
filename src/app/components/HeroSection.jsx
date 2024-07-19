bb98a5f616a3177114226c28243595a620fbcbaa
lilian-su <liliank.497@gmail.com>
2024-07-18 22:14:35 -0600

    update

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -50,11 +50,11 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+          <div className=" w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/images/hero-image.png"
-              alt="hero image"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              src="/images/profile-pic.jpeg"
+              alt="profile img"
+              className="rounded-full absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={300}
               height={300}
             />
